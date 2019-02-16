d6e4141ab111d5f7de21e2140c229e839b0d3dcf
Ibnu syawal <46734417+ibnusyawall@users.noreply.github.com>
2019-02-17 00:00:07 +0700

    Add files via upload

---
+++ mytools.js
@@ -0,0 +1,144 @@
+/* Coded By Ibnu Syawall */
+/* My Tools */
+/* Say Alhamdulillah :) */
+
+// ---------- module ----------
+
+var a = require('request') ;
+var b = require('readline');
+var c = require('shelljs') ;
+var d = require('colors')  ;
+
+// ---------- warna ----------
+
+d.setTheme({
+    kuning : 'yellow',
+    hijau  : 'green' ,
+    biru   : 'blue'  ,
+    merah  : 'red'
+})
+
+// ---------- shell ----------
+
+c.exec('clear off');
+
+// ---------- garid ----------
+
+garis = ' =========================== '
+
+// ---------- banner ----------
+
+c.echo('            _           _')
+c.echo('  _____ _ _| |_ ___ ___| |___')
+c.echo(' |     | | |  _| . | . | |_ -|')
+c.echo(' |_|_|_|_  |_| |___|___|_|___|')
+c.echo('       |___| ')
+c.echo('')
+c.echo("     C" + " : " + "I".merah + "bnu " + "S".merah + "yawall")
+c.echo("   407 Authentic Exploit")
+c.echo(garis); c.echo('');
+
+// ---------- input ----------
+
+const masuk = b.createInterface({
+    input : process.stdin,
+    output: process.stdout
+});
+
+// ---------- list ----------
+
+pa = ' ['.kuning, gar = '] '.kuning
+
+c.echo(pa + '1' + gar + 'DNS Lookup        ' + pa + '6' + gar + 'Whois Lookup     ' + pa + '11' + gar + 'HTTP Header Check')
+c.echo(pa + '2' + gar + 'Reverse DNS       ' + pa + '7' + gar + 'GeoIp Lookup     ' + pa + '12' + gar + 'Extract Link From Web')
+c.echo(pa + '3' + gar + 'Find DNS Record   ' + pa + '8' + gar + 'Reverse Ip Lookup' + pa + '13' + gar + 'Trace Route')
+c.echo(pa + '4' + gar + 'Find Shared DNS   ' + pa + '9' + gar + 'TCP Port Scan    ' + pa + '99' + gar + 'Quit');
+c.echo(pa + '5' + gar + 'Zone Transfer Test' + pa + '10' + gar + 'Subnet Lookup   '); c.echo("");
+
+// ---------- url ----------
+
+var url = ['https://api.hackertarget.com/dnslookup/?q=', 'https://api.hackertarget.com/reversedns/?q=',
+           'https://api.hackertarget.com/hostsearch/?q=', 'https://api.hackertarget.com/findshareddns/?q=',
+           'https://api.hackertarget.com/zonetransfer/?q=', 'https://api.hackertarget.com/whois/?q=',
+           'https://api.hackertarget.com/geoip/?q=', 'https://api.hackertarget.com/reverseiplookup/?q=',
+           'https://api.hackertarget.com/nmap/?q=', 'https://api.hackertarget.com/subnetcalc/?q=',
+           'https://api.hackertarget.com/httpheaders/?q=', 'https://api.hackertarget.com/pagelinks/?q=',
+           'https://api.hackertarget.com/mtr/?q=']
+
+// ---------- nganu ----------
+
+masuk.question(' [my#tools] | : ', (tampil) => {
+     c.echo('');c.echo(garis);
+     if (`${tampil}` == 1){
+         masuk.question(pa + '!'.merah + gar + ' Enter Domain       : ', (aa) => {
+             c.echo(garis);
+             c.echo(pa + '√'.biru + gar + ' Harap Tunggu ..');c.echo(garis); c.exec('curl ' + url[0] + `${aa}`); masuk.close();
+         });
+     }else if (`${tampil}` == 2){
+         masuk.question(pa + '!'.merah + gar + ' Ip / Domain        : ', (bb) => {
+             c.echo(garis);
+             c.echo(pa + '√'.biru + gar + ' Harap Tunggu ..');c.echo(garis); c.exec('curl ' + url[1] + `${bb}`); masuk.close();
+          });
+     }else if (`${tampil}` == 3){
+         masuk.question(pa + '!'.merah + gar + ' Enter Domain       : ', (cc) => {
+            c.echo(garis);
+            c.echo(pa + '√'.biru + gar + ' Harap Tunggu .. ');c.echo(garis); c.exec('curl ' + url[2] + `${cc}`); masuk.close();
+         });
+     }else if (`${tampil}` == 4){
+         masuk.question(pa + '!'.merah + gar + ' Enter NS of Domain : ', (dd) => {
+            c.echo(garis);
+            c.echo(pa + '√'.biru + gar + ' Harap Tunggu .. ');c.echo(garis); c.exec('curl ' + url[3] + `${dd}`); masuk.close();
+         });
+     }else if (`${tampil}` == 5){
+         masuk.question(pa + '!'.merah + gar + ' Enter Domain       : ', (ee) => {
+            c.echo(garis);
+            c.echo(pa + '√'.biru + gar + ' Harap Tunggu .. ');c.echo(garis); c.exec('curl ' + url[4] + `${ee}`); masuk.close();
+         });
+     }else if (`${tampil}` == 6){
+         masuk.question(pa + '!'.merah + gar + ' Ip / Domain        : ', (ff) => {
+            c.echo(garis);
+            c.echo(pa + '√'.biru + gar + ' Harap Tunggu .. ');c.echo(garis); c.exec('curl ' + url[5] + `${ff}`); masuk.close();
+         });
+     }else if (`${tampil}` == 7){
+         masuk.question(pa + '!'.merah + gar + ' Enter Ip Address   : ', (gg) => {
+            c.echo(garis);
+            c.echo(pa + '√'.biru + gar + ' Harap Tunggu .. ');c.echo(garis); c.exec('curl ' + url[6] + `${gg}`); masuk.close();
+         });
+     }else if (`${tampil}` == 8){
+         masuk.question(pa + '!'.merah + gar + ' Enter Ip Address   : ', (hh) => {
+            c.echo(garis);
+            c.echo(pa + '√'.biru + gar + ' Harap Tunggu .. ');c.echo(garis); c.exec('curl ' + url[7] + `${hh}`); masuk.close();
+         });
+     }else if (`${tampil}` == 9){
+         masuk.question(pa + '!'.merah + gar + ' Ip / Domain        : ', (ii) => {
+            c.echo(garis);
+            c.echo(pa + '√'.biru + gar + ' Harap Tunggu .. ');c.echo(garis); c.exec('curl ' + url[8] + `${ii}`); masuk.close();
+         });
+     }else if (`${tampil}` == 10){
+         masuk.question(pa + '!'.merah + gar + ' Enter Ip Address   : ', (jj) => {
+            c.echo(garis);
+            c.echo(pa + '√'.biru + gar + ' Harap Tunggu .. ');c.echo(garis); c.exec('curl ' + url[9] + `${jj}`); masuk.close();
+         });
+     }else if (`${tampil}` == 11){
+         masuk.question(pa + '!'.merah + gar + ' Enter Domain       : ', (kk) => {
+            c.echo(garis);
+            c.echo(pa + '√'.biru + gar + ' Harap Tunggu .. ');c.echo(garis); c.exec('curl ' + url[10] + `${kk}`); masuk.close();
+         });
+     }else if (`${tampil}` == 12){
+         masuk.question(pa + '!'.merah + gar + ' Enter Domain       : ', (ll) => {
+            c.echo(garis);
+            c.echo(pa + '√'.biru + gar + ' Harao Tunggu .. ');c.echo(garis); c.exec('curl ' + url[11] + `${ll}`); masuk.close();
+         });
+     }else if (`${tampil}` == 13){
+         masuk.question(pa + '!'.merah + gar + ' Ip / Domain        : ', (mm) => {
+            c.echo(garis);
+            c.echo(pa + '√'.biru + gar + ' Harap Tunggu .. ');c.echo(garis); c.exec('curl ' + url[12] + `${mm}`); masuk.close();
+         });
+     }else if (`${tampil}` == 99){
+            c.echo(pa + '√'.biru + gar + " Harap Tunggu .. "); c.exec('exit'); c.exec('clear off'); c.echo(' Thanks for using this tools');
+            c.echo('      [ my#tools ] > bye     '); c.exec('clear off && ls'); masuk.close();
+     }else {
+            c.echo(' Masukan input dengan benar !'); c.exec('exit'); c.exec('clear off && ls'); masuk.close();
+     }; });
+
+// ---------- exit ----------
